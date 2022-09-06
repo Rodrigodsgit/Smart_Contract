7d1a5ba54dd045a03c3073dc6d184de46e56464c
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2022-09-06 00:43:02 -0300

    Adição de novas funções 	modified:   contracts/VendingMachine.sol

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -1,27 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract VendingMachine {
+contract E_Mile {
+
     address public owner;
-    mapping(address => uint256) public milesBalance;
-    mapping(address => uint256) public etherBalance;
+    mapping(address => uint256) private milesBalance;
 
     constructor() {
         owner = msg.sender;
-
         milesBalance[address(this)] = 100;
-        etherBalance[address(this)] = 10;
+
     }
 
-    function GetMiles() public view returns (uint256) {
+    function getMiles() public view returns (uint256) {
         return milesBalance[address(this)];
     }
 
-    function GetEtherBalance() public view returns (uint256) {
-        return etherBalance[address(this)];
+    function getBalance() public view returns (uint256){
+      return address(this).balance;
     }
 
-    function RestokMiles(uint256 amount) public {
+    function getEtherBalanceClient() public view returns (uint256) {
+        return msg.sender.balance;
+    }
+
+    function getBalanceMilesClient() public view returns (uint256){
+      return milesBalance[msg.sender];
+    }
+
+    function restokMiles(uint256 amount) public {
         require(
             msg.sender == owner,
             unicode"Somente o proprietário da máquina de venda automática pode reabastecer."
@@ -29,18 +36,22 @@ contract VendingMachine {
         milesBalance[address(this)] += amount;
     }
 
-    function RestokEther(uint256 amount) public {
+    function RestokEther() public payable {
         require(
             msg.sender == owner,
             unicode"Somente o proprietário da máquina de venda automática pode reabastecer."
         );
-        etherBalance[address(this)] += amount;
+        require(
+            msg.value >=  1 gwei,
+            unicode"O valor mínimo para reabastecer é 1 gwei"
+        );
+
     }
 
     //TODO: Definir quando pode fazer saque de ether
     function Withdrawal() public {}
 
-    //TODO: Definir valor de compra/venda
+    //TODO: Definir valor de compra
     function PriceMiles() public {}
 
     function PurchaseMiles(uint256 amount) public payable {
@@ -54,7 +65,35 @@ contract VendingMachine {
             unicode"Não há milhas suficientes na máquina de venda automática."
         );
 
+        require(
+          amount > 0,
+          unicode"O valor mínimo para compra de milhas deve ser igual ou superior a 1"
+        );
+
         milesBalance[address(this)] -= amount;
         milesBalance[msg.sender] += amount;
     }
+
+    function PurchaseEther(uint256 amount) public payable {
+        require(
+          address(this).balance >= amount * 1 gwei,
+          unicode"Desculpe, no momento não há ether suficiente para comprar suas milhas, tente novamente mais tarde."
+        );
+
+        require(
+          milesBalance[msg.sender] >= amount,
+          unicode"Você não possui essa quantidade de milhas."
+        );
+
+        require(
+          amount > 0,
+          unicode"O valor mínimo para venda de milhas deve ser igual ou superior a 1"
+        );
+       milesBalance[address(this)] += amount;
+       milesBalance[msg.sender] -= amount;
+
+       payable(msg.sender).transfer( amount * 1 gwei);
+    }
+
+
 }
