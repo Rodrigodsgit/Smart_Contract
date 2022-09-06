3ca45a800cfe4499d746f465847939db37e46985
Luis Carlos <roxisxprol@gmail.com>
2022-09-05 22:32:49 -0300

    Add Documentacao

--- contracts/UserContract.sol
+++ contracts/UserContract.sol
@@ -2,9 +2,28 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract UserContract {
+    uint public milesAmount;
+
+    mapping(uint => Mile) public miles;
+    
     constructor() {}
 
-    function PurchaseToken() public {}
+    struct Mile { 
+        uint id;       
+        uint price;
+        address payable owner;
+        bool sold;
+        bool purchasinhg;
+    }
+    
+    function PurchaseMiles(uint id) public payable {
+        Mile memory mile = miles[id];
 
-    function SaleToken() public {}
+        address payable seller = mile.owner;
+
+        require(msg.value >= mile.price,
+            unicode"")
+    }
+
+    function SoldIn() public {}
 }
