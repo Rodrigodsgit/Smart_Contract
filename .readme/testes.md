7388452b734a73bc521ffd81b5d32869df1c0ec5
Luis Carlos <roxisxprol@gmail.com>
2022-09-07 21:10:32 -0300

    Setup inicial tests

--- .readme/testes.md
+++ .readme/testes.md
@@ -1,6 +1,16 @@
+---
+output: 
+  html_document:
+     css: ./readme.css
+     self_contained: no
+---
+
 [Voltar](../Readme.md)
 
+## Implemntacao dos testes 
 
-### Implemntacao dos testes 
+![teste inicial {.tests-img}](../assets/test_inicial.png)
 
-![teste inicial](../assets/test_inicial.png)
+## Testes Funcionais 
+
+Teste do Lucas 
--- test/vending_machine.js
+++ test/vending_machine.js
@@ -1,13 +1,26 @@
 const VendingMachine = artifacts.require("VendingMachine");
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
-contract("VendingMachine", function (/* accounts */) {
-  it("should assert true", async function () {
-    await VendingMachine.deployed();
-    return assert.isTrue(true);
-  });
+contract('VendingMachine', (accounts) => {
+  let instance;
+
+  // Test deploy contract
+  beforeEach('should be able to show if the contract has been deployed', async () => {
+    instance = await VendingMachine.deployed();
+  })
+
+  it('should be able to show the amount of miles in the wallet', async () => {
+    const miles = await VendingMachine.getMiles(); 
+    assert.isAtLeast(miles, 0, 'Quantidade de Milhas maior que 0')
+  })
+
+  it('should be able to show the selling price of miles', async () => {
+    const priceSell = await VendingMachine.getPriceSell(); 
+    assert.isAtLeast(priceSell, 0, `O preco de venda e ${priceSell}`)
+  })
+
+  it('should be able to make a mile sale', async () => {
+      await VendingMachine.sellMiles(1); 
+    // console.log(sellMiles)
+  })
+
 });
